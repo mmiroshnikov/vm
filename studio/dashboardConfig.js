a7d90bf9920321d68bdf90e202f75d9c754cc1af
mmiroshnikov <41021818+mmiroshnikov@users.noreply.github.com>
2019-11-30 13:23:29 -0800

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,57 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-gatsby-portfolio'
+      }
+    },
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '5de2ddcc9e61a549d6207296',
+                  title: 'Sanity Studio',
+                  name: 'vm-studio',
+                  apiId: '81b5a1e1-389d-4d1d-9669-755c1b749ea2'
+                },
+                {
+                  buildHookId: '5de2ddcd07c968b9843dabc7',
+                  title: 'Portfolio Website',
+                  name: 'vm-web',
+                  apiId: '1368952a-892d-46d4-84a5-7baa781702bf'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/mmiroshnikov/vm',
+            category: 'Code'
+          },
+          {
+            title: 'Frontend',
+            value: 'https://vm-web.netlify.com',
+            category: 'apps'
+          }
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent projects', order: '_createdAt desc', types: ['sampleProject']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'bnu7ijsr',
+    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
+  }
+}
